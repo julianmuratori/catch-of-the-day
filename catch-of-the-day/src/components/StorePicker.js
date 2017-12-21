c63a8a3964bb6c6e5c2c3b250de6ef0fe8570d96
Julian Muratori <jmuratori@ujafed.org>
2017-12-21 11:18:05 -0500

    lesson 13

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,20 +1,33 @@
 // let's go!
 import React from 'react';
+import { getFunName } from '../helpers';
 
 
 class StorePicker extends React.Component {
+    // constructor() {
+    //     super();
+    //     this.goToStore = this.goToStore.bind(this);
+    // }
 
+
+
+    goToStore(event) {
+        // grabs text from input box
+        event.preventDefault();
+        console.log(this.storeInput.value);
+        // transition from / to /store/:storeId
+    }
     render() {
 
         return (
 
-            <form action="" className="store-selector">
+            <form action="" className="store-selector" onSubmit={(e) => this.goToStore(e)}>
                 <h2>Please enter a store</h2>
-                <input type="text" required placeholder="Store Name"/>
+                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input}} />
                 <button type="submit">Visit Store!</button>
             </form>
 
-        )
+        ) 
     } 
 }
 
