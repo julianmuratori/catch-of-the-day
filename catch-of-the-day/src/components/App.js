31c46f413042ec4791619f7007792f332f9b4c55
Julian Muratori <jmuratori@ujafed.org>
2017-12-22 14:43:54 -0500

    learning more about state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
     constructor() {
         super();
         
         this.addFish = this.addFish.bind(this);
+        this.loadSamples = this.loadSamples.bind(this);
         // get initial state
         this.state = {
             fishes: {},
@@ -28,6 +31,11 @@ class App extends React.Component {
         this.setState({ fishes });
     }
 
+    loadSamples() {
+        this.setState({
+            fishes: sampleFishes
+        })
+    }
 
     render() {
         return (
@@ -35,10 +43,15 @@ class App extends React.Component {
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market"/>
-
+                    <ul className="list-of-fishes">
+                        {
+                            Object.keys(this.state.fishes)
+                                .map(key => <Fish key={key} details={this.state.fishes[key]} />)
+                        }
+                    </ul>
                 </div>
                 <Order />
-                <Inventory addFish={this.addFish} />
+                <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
             </div>
 
         )
